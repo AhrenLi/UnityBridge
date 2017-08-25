3d9999615daf48f7e630dc56eb8c1a0d59688dd2
ahrenli <ahrenli@foxmail.com>
2017-08-25 12:20:10 +0800

    Initial commit

---
+++ UnityBridge.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace UnityBridge
+{
+    public partial class UnityBridge : ServiceBase
+    {
+        public UnityBridge()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            using (StreamWriter sw = new StreamWriter("c://log.txt", true))
+            {
+                sw.WriteLine(DateTime.Now.ToString() + "  On Start ");
+            }
+        }
+
+        protected override void OnStop()
+        {
+            using (StreamWriter sw = new StreamWriter("c://log.txt", true))
+            {
+                sw.WriteLine(DateTime.Now.ToString() + "  On Stop ");
+            }
+        }
+    }
+}
